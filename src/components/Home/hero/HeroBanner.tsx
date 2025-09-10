e6acb7a33819d0746314bacf1173db1ba1527b3e
Nhat HO NGUYEN <nhat@tkorp.com>
2025-09-10 22:04:10 +0200

    ✨ refactor(images): replace banner background image and update references
    - Added a new background image (Background_pic.jpg) to the project.
    - Removed the old banner background image (banner-bg-img.png).
    - Updated the HeroBanner component to use the new background image.
    - Deleted outdated commit conventions and TypeScript guidelines documents.
    - Added @tailwindcss/postcss as a dev dependency in package.json.

--- src/components/Home/hero/HeroBanner.tsx
+++ src/components/Home/hero/HeroBanner.tsx
@@ -4,7 +4,7 @@ export default function HeroBanner() {
   return (
     <div className="w-full h-64 sm:h-72 lg:h-80">
       <Image
-        src={'/images/banner-bg-img.png'}
+        src={'/images/Background_pic.jpg'}
         alt="banner-img"
         width={1080}
         height={267}
