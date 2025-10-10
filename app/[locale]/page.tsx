d9ba22d6f2b27b89d40406c5bc78f5c3dc59d96a
HO NGUYEN Nhat-Quan <152589241+NhatHONGUYEN@users.noreply.github.com>
2025-10-10 09:21:48 +0200

    fix: update dependencies in package.json and package-lock.json for improved stability

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,7 +5,6 @@ import Education from '@/components/Home/Education';
 import Experience from '@/components/Home/Experience';
 import Header from '@/components/Home/Header';
 import Hero from '@/components/Home/hero/Hero';
-
 import Projects from '@/components/Home/Projects';
 
 export default function Home() {
