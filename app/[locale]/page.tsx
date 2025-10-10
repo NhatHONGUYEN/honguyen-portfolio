b959d8d0667732c836902865e4686199cf0949fb
HO NGUYEN Nhat-Quan <152589241+NhatHONGUYEN@users.noreply.github.com>
2025-10-10 09:47:54 +0200

    refactor: remove unused Contact components and clean up Home layout

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,7 +5,6 @@ import Experience from '@/components/Home/Experience';
 import Header from '@/components/Home/Header';
 import Hero from '@/components/Home/hero/Hero';
 import Projects from '@/components/Home/Projects';
-import ContactSection from '@/components/Home/contact/ContactSection';
 
 export default function Home() {
   return (
@@ -22,7 +21,7 @@ export default function Home() {
       <Divider />
       <Projects />
       <Divider />
-      <ContactSection />
+
       <Divider />
     </>
   );
