4acc04d39aaf5f50cfc07fd7d9b86cf2cd3950bb
HO NGUYEN Nhat-Quan <152589241+NhatHONGUYEN@users.noreply.github.com>
2025-10-10 09:27:40 +0200

    refactor: comment out unused ScreenSize import and component for cleaner layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,7 +7,7 @@ import { routing } from '@/i18n/routing';
 import ClientWrapper from '../../src/lib/ClientWrapper';
 import Footer from '@/components/Home/Footer';
 import { Toaster } from '@/components/ui/sonner';
-import { ScreenSize } from '@/components/ScreenSize';
+// import { ScreenSize } from '@/components/ScreenSize';
 import ScrollToTopButton from '@/components/ui/buttons/ScrollToTopButton';
 
 const workSans = Work_Sans({
@@ -76,7 +76,7 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} className="scroll-smooth" suppressHydrationWarning>
       <body className={`${workSans.variable} antialiased bg-muted `}>
-        <ScreenSize />
+        {/* <ScreenSize /> */}
         <ClientWrapper messages={messages} locale={locale}>
           <div className="relative z-50">
             {children}
